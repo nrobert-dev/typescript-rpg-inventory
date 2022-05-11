ef294cdac7b85333c644f84c7fd8d77d6545dfee
robertnechitelea <robert.nechitelea@toluna.com>
2022-05-11 15:07:11 +0300

    Updates

---
+++ precache-manifest.ccd825c2b21dc8e9f32def056bab0437.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23de5e3f05af8b7fbe8de05c77f5e8bf",
+    "url": "/typescript-rpg-inventory/index.html"
+  },
+  {
+    "revision": "9100861a1cb904680100",
+    "url": "/typescript-rpg-inventory/static/css/main.3386aae1.chunk.css"
+  },
+  {
+    "revision": "279b3911550a2b6b1cec",
+    "url": "/typescript-rpg-inventory/static/js/2.a00cd5a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typescript-rpg-inventory/static/js/2.a00cd5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9100861a1cb904680100",
+    "url": "/typescript-rpg-inventory/static/js/main.18a72f9b.chunk.js"
+  },
+  {
+    "revision": "73c6ec5aa49afd9d15d2",
+    "url": "/typescript-rpg-inventory/static/js/runtime-main.664b8b66.js"
+  },
+  {
+    "revision": "599449a20c027bb66dcc89ee34e3bfc1",
+    "url": "/typescript-rpg-inventory/static/media/background.599449a2.jpg"
+  },
+  {
+    "revision": "6b92059653700107fd14d21e51467647",
+    "url": "/typescript-rpg-inventory/static/media/fireball-red-2.6b920596.png"
+  },
+  {
+    "revision": "edc46fe7db6cea409d06a2f7b0980ea7",
+    "url": "/typescript-rpg-inventory/static/media/fireball-sky-2.edc46fe7.png"
+  },
+  {
+    "revision": "618517c74e2d153cecfb2cee00231be1",
+    "url": "/typescript-rpg-inventory/static/media/heal-royal-1.618517c7.png"
+  },
+  {
+    "revision": "ea84d0fd30ab7c53184e387fbff831d9",
+    "url": "/typescript-rpg-inventory/static/media/human.ea84d0fd.png"
+  },
+  {
+    "revision": "669922386aa1b4318c5544af6eb18f94",
+    "url": "/typescript-rpg-inventory/static/media/main.66992238.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rpg-inventory/precache-manifest.cd126832c65129f1ff912daf580dccd6.js"
+  "/typescript-rpg-inventory/precache-manifest.ccd825c2b21dc8e9f32def056bab0437.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rpg-inventory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/typescript-rpg-inventory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
