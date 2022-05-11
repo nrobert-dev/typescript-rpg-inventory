9674e4e0498c2e3ac4d9e79a116b7a134d77dc8b
robertnechitelea <robert.nechitelea@toluna.com>
2022-05-11 14:57:43 +0300

    Updates

---
+++ precache-manifest.cd126832c65129f1ff912daf580dccd6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8fc125a6e68dacccd103b1cf4a89ff",
+    "url": "/rpg-inventory/index.html"
+  },
+  {
+    "revision": "dff376928412dc256980",
+    "url": "/rpg-inventory/static/css/main.4fcd9d23.chunk.css"
+  },
+  {
+    "revision": "279b3911550a2b6b1cec",
+    "url": "/rpg-inventory/static/js/2.a00cd5a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rpg-inventory/static/js/2.a00cd5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff376928412dc256980",
+    "url": "/rpg-inventory/static/js/main.2e0bd940.chunk.js"
+  },
+  {
+    "revision": "d77609f45b4b177e9f14",
+    "url": "/rpg-inventory/static/js/runtime-main.3186b746.js"
+  },
+  {
+    "revision": "599449a20c027bb66dcc89ee34e3bfc1",
+    "url": "/rpg-inventory/static/media/background.599449a2.jpg"
+  },
+  {
+    "revision": "6b92059653700107fd14d21e51467647",
+    "url": "/rpg-inventory/static/media/fireball-red-2.6b920596.png"
+  },
+  {
+    "revision": "edc46fe7db6cea409d06a2f7b0980ea7",
+    "url": "/rpg-inventory/static/media/fireball-sky-2.edc46fe7.png"
+  },
+  {
+    "revision": "618517c74e2d153cecfb2cee00231be1",
+    "url": "/rpg-inventory/static/media/heal-royal-1.618517c7.png"
+  },
+  {
+    "revision": "ea84d0fd30ab7c53184e387fbff831d9",
+    "url": "/rpg-inventory/static/media/human.ea84d0fd.png"
+  },
+  {
+    "revision": "669922386aa1b4318c5544af6eb18f94",
+    "url": "/rpg-inventory/static/media/main.66992238.png"
+  }
+]);
